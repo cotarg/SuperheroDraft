b8894facc98074bb6015b6908fde58c8f7ab5b22
Rowan Cota <cota.rg@gmail.com>
2016-08-09 11:30:00 -0700

    set up matches table, first pass. this may change.

--- db/migrate/20160808175219_create_matches.rb
+++ db/migrate/20160808175219_create_matches.rb
@@ -1,6 +1,12 @@
 class CreateMatches < ActiveRecord::Migration
   def change
     create_table :matches do |t|
+      t.integer :host_id
+      t.integer :player_one
+      t.integer :player_two
+      t.integer :player_three
+      t.integer :player_four
+      
 
       t.timestamps null: false
     end
