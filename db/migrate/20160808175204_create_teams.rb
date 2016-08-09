147a6f586ec6437b8a8f5e3826f23881376fb5bf
Rowan Cota <cota.rg@gmail.com>
2016-08-09 11:33:55 -0700

    set up team table first draft.

--- db/migrate/20160808175204_create_teams.rb
+++ db/migrate/20160808175204_create_teams.rb
@@ -1,6 +1,22 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
+      t.integer :player_id
+      
+      t.string :hero_one
+      t.string :hero_two
+      t.string :hero_three
+      t.string :hero_four
+      t.string :hero_five
+      t.string :hero_six
+
+      t.string :villain_one
+      t.string :villain_two
+      t.string :villain_three
+      t.string :villain_four
+
+      t.string :cover_url
+      t.text :pitch
 
       t.timestamps null: false
     end
