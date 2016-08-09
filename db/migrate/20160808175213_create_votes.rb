d96c7af5e3dc74b167cb2012b0e517af2cc9e976
Rowan Cota <cota.rg@gmail.com>
2016-08-09 10:26:31 -0700

    set up votes table migration.

--- db/migrate/20160808175213_create_votes.rb
+++ db/migrate/20160808175213_create_votes.rb
@@ -1,6 +1,8 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
+      t.integer :team_id # the team being upvoted
+      t.integer :user_id # the user making the upvote
 
       t.timestamps null: false
     end
