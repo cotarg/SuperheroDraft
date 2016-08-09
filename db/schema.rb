b4cdf505277504f17685830b2b99b99e6a023fdd
Rowan Cota <cota.rg@gmail.com>
2016-08-09 12:51:25 -0700

    databse set up, merging to master.

---
+++ db/schema.rb
@@ -0,0 +1,64 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160808175219) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "matches", force: :cascade do |t|
+    t.integer  "host_id"
+    t.integer  "player_one"
+    t.integer  "player_two"
+    t.integer  "player_three"
+    t.integer  "player_four"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.integer  "player_id"
+    t.string   "hero_one"
+    t.string   "hero_two"
+    t.string   "hero_three"
+    t.string   "hero_four"
+    t.string   "hero_five"
+    t.string   "hero_six"
+    t.string   "villain_one"
+    t.string   "villain_two"
+    t.string   "villain_three"
+    t.string   "villain_four"
+    t.string   "cover_url"
+    t.string   "playlist_url"
+    t.text     "pitch"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",   null: false
+    t.string   "uid",        null: false
+    t.string   "name",       null: false
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "team_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
