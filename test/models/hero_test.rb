090483e715a24822672e69fe8159be98d3802d85
Rowan Cota <cota.rg@gmail.com>
2016-08-10 15:42:55 -0700

    added some tests and scaffolding for them.

--- test/models/hero_test.rb
+++ test/models/hero_test.rb
@@ -16,4 +16,17 @@ class HeroTest < ActiveSupport::TestCase
     assert_equal test_hero.name, "clive"
   end
 
+  test "a hero has a description" do
+    # set up a test case? use fixtures?
+  end
+
+  test "a hero has an image" do
+    # the model should contain a field for image url
+  end
+
+  test "a hero has an key/value pair representing id and supplying api" do
+    # there should be a field for this hash
+  end
+
+
 end
