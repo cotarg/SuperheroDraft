d222255f7d64605bba439bfb07634fc20f06e914
Rowan Cota <cota.rg@gmail.com>
2016-08-11 10:43:38 -0700

    committing for complete history.

--- test/models/hero_test.rb
+++ test/models/hero_test.rb
@@ -1,31 +1,38 @@
 require 'test_helper'
 
 class HeroTest < ActiveSupport::TestCase
+  extend Minitest::Spec::DSL
+
+  let(:hero_one) { Hero.new("Clive", "Normal Guy", "A totally normal guy. Person Man.", "yahoo.com", "test data", "M") }
 
   test "it exists" do
     refute_nil Hero
   end
 
   test "it can instantiate" do
-    test_hero = Hero.new("clive")
-    refute_nil test_hero
+    refute_nil hero_one
   end
 
   test "a hero has a name" do
-    test_hero = Hero.new("clive")
-    assert_equal test_hero.name, "clive"
+    assert_equal hero_one.name, "Clive"
+  end
+
+  test "a hero has a list of aliases" do
+    refute_nil hero_one.aliases
   end
 
   test "a hero has a description" do
-    # set up a test case? use fixtures?
+    refute_nil hero_one.description
   end
 
   test "a hero has an image" do
     # the model should contain a field for image url
+    refute_nil hero_one.image_url
   end
 
   test "a hero has an key/value pair representing id and supplying api" do
     # there should be a field for this hash
+    refute_nil hero_one.id_obj
   end
 
 
