213cbe00796a760534f26c02ad1660e626e49556
Rowan Cota <cota.rg@gmail.com>
2016-08-13 13:23:42 -0700

    working on getting a working user creation and login.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def create
+    @user = User.new(user_create_params[:user])
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+
+  def destroy
+  end
+
+  private
+
+  def user_create_params
+    params.permit(user: [:name, :email, :password, :password_confirmation])
+  end
+
 end
