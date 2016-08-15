f008be3afcc0e47ebec3fbe05de4e58c8046b0d0
Rowan Cota <cota.rg@gmail.com>
2016-08-15 14:28:59 -0700

    can finally find and display all teams belonging to a user when user is hardcoded to find.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,27 @@
 class TeamsController < ApplicationController
+
+def index
+  # @teams = Team.where(user_id: 12) || Team.new() (This works, and is now the format for these.)
+
+  render :index
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def show
+end
+
+def update
+end
+
+def destroy
+end
+
 end
