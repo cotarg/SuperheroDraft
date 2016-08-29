8c7ba721d746254635136914735ebd1c0f1145b8
Rowan Cota <cota.rg@gmail.com>
2016-08-29 12:36:01 -0700

    fixing merged error, I assume.

--- db/migrate/20160818215234_add_match_title_and_stakes_to_matches_model.rb
+++ db/migrate/20160818215234_add_match_title_and_stakes_to_matches_model.rb
@@ -1,5 +1,6 @@
 class AddMatchTitleAndStakesToMatchesModel < ActiveRecord::Migration
   def change
-    add_column :users, :email, :string
+    add_column :matches, :name, :string
+    add_column :matches, :stakes, :string
   end
 end
